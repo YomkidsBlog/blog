1985a8833a3406f733df654638226543d13159c2
YomkidsBlog <121500942+YomkidsBlog@users.noreply.github.com>
2022-12-27 15:14:07 +0100

    Add files via upload

---
+++ blog/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yomkid's Blog | Home</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <div class="navbar">
+                <div class="logo">Yomkid's Blog</div>
+                
+                    <div class="navlinks">
+                        <a href="#"><li>Home</li></a>
+                        <a href="#"><li>About</li></a>
+                        <a href="#"><li>Media</li></a>
+                        <a href="#"><li>Marketplace</li></a>
+                    </div>
+                
+            </div>
+        </nav>
+        <div class="title-container">
+            <hr>
+            <h1 class="title">Mayor Longtime Friends From The Other Sides From The Other Sides From The Other Side</h1>
+            <hr>
+
+
+            <div class="post-author-date">
+                <div class="author-name-pics">
+                    <div><img src="assets/img/author.jpg" alt="Author" class="author-pics" width="50" style="margin-right: 10px;"></div>
+                    <div>By Odewaye Mayomi</div>
+                </div>
+                
+                <div><em>Posted On: 21/12/2022</em></div>
+            </div>
+        </div>
+
+       
+
+        <div class="content">
+
+            <div class="featured">
+                <img src="./assets/img/aboutus.png" alt="Featured Image" class="featured-image">
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Eligendi, soluta. Pariatur aperiam tenetur alias, vero, atque fugit porro officia 
+                totam consequatur, ad voluptatum quos ipsum dolorem amet. Doloremque deserunt molestiae 
+                illum cumque. Esse similique error atque quam architecto impedit ea, quidem nobis. 
+                Eaque non quasi, ducimus ipsam distinctio quod excepturi?\Lorem ipsum dolor sit amet, 
+                consectetur adipisicing elit. Deserunt laudantium totam sit molestias minus 
+                deleniti sint laboriosam, neque earum itaque expedita atque reiciendis quia debitis, 
+                quos minima natus, nemo fuga ut perspiciatis assumenda possimus recusandae eius quisquam? 
+                Eveniet voluptas non in eligendi? Tenetur temporibus sequi veniam delectus voluptate 
+                voluptates doloremque maxime. Nam repellendus accusantium expedita, non magnam deleniti corrupti. 
+                Ipsam consectetur possimus accusamus quis voluptatem atque molestias aliquam, nemo recusandae molestiae 
+                cumque iusto, eligendi soluta et velit unde expedita odio quam! Dolores perferendis officiis, nam adipisci 
+                omnis doloremque unde praesentium eos dignissimos ipsam, assumenda enim quis, numquam sequi tempora reiciendis!</p>
+        
+                <hr>
+                <div class="author-container">
+                    <h2 class="about-head">ABOUT AUTHOR</h2>
+                    <div class="author">
+                        
+                        <img src="assets/img/author.jpg" alt="Author" width="170" class="clearfix">
+                        <h2>ODEWAYE MAYOMI P.</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Laudantium sapiente voluptatum minus sequi architecto delectus! Hic corrupti 
+                        doloribus maxime quam sit velit harum aut reprehenderit ut! Accusantium ex illo 
+                        quisquam vel aspernatur rem, cupiditate est architecto magnam eos quod harum provident 
+                        bitis facilis repellat ea officia?
+                        
+                       </p>
+                        
+                    </div>
+
+                    <div class="author-contact">
+                        sdwdsfcadfafasfa
+                    </div>
+                    
+        
+                </div>
+                
+        
+        
+        </div>
+    </div>
+    
+</body>
+</html>
