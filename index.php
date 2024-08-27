c516a27aff9996ff1a4afffc17f107f9205a9492
YomkidsBlog <121500942+YomkidsBlog@users.noreply.github.com>
2024-08-27 11:17:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
   </head>
   <body>
     <h1>Welcome to Testing environment D-ONE the Boss</h1>
+    <div>
+      <h2>Testing Environment</h2>
+Welcome D-One, this is a testing environmnent, work is going underground once we start making changes it will appear hear as soon as possible You will be seeing the progress of the work here once it pushed here.
+
+So no cause for alarm at all.
+    </div>
   </body>
 </html>
